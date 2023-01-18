2c67631154367d1bf924ec85a5a16743dd194b08
unknown <suvammeher95@gmail.com>
2023-01-18 18:07:02 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './core/admin/admin.component';
+import { ProductComponent } from './core/product/product.component';
+import { CartComponent } from './core/cart/cart.component';
+import { Route, RouterModule } from '@angular/router';
+
+const allRoutes: Route[] = [
+  { path: 'product', component: ProductComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'admin', component: AdminComponent },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, AdminComponent, ProductComponent, CartComponent],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(allRoutes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
